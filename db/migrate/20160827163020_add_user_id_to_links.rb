445d3e4e9c3d0c68e1f72d99139ee7d22fea6d1e
Don Warner <dwarner1221@gmail.com>
2016-08-27 11:33:06 -0500

    Add association between contact and user

---
+++ db/migrate/20160827163020_add_user_id_to_links.rb
@@ -0,0 +1,6 @@
+class AddUserIdToLinks < ActiveRecord::Migration
+  def change
+    add_column :contacts, :user_id, :integer
+    add_index :contacts, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827160550) do
+ActiveRecord::Schema.define(version: 20160827163020) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20160827160550) do
     t.string   "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "contacts", ["user_id"], name: "index_contacts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
